3f19c79a21e2c4960aa2ecb302ddd92107e73174
NguyenHoang1602 <sakuraimusic123@gmail.com>
2023-10-20 10:00:01 +0700

    Container

--- App.js
+++ App.js
@@ -12,113 +12,26 @@
 
 // In App.js in a new project
 
-import  React,{ useState }  from 'react';
-import { View, Text, StatusBar, Image } from 'react-native';
-import { COLORS, SIZES } from './src/constants/theme.jsx';
-import AppIntroSlider from "react-native-app-intro-slider";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Router from './src/router/router';
 
-const slides = [
-  {
-    id: 1,
-    title: 'Discover Best Places',
-    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
-    image: require('./src/assets/images/Onboarding/vecteezy_man-search-for-hiring-job-online-from-laptop-human_.jpg')
-  },
-  {
-    id: 2,
-    title: 'Choose A Tasty Dish',
-    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
-    image: require('./src/assets/images/Onboarding/vecteezy_a-salesperson-holds-a-megaphone-in-hand-and-announces-a_.jpg')
-  },
-  {
-    id: 3,
-    title: 'Pick Up The Delivery',
-    description: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"',
-    image: require('./src/assets/images/Onboarding/7566.jpg')
-  }
-]
-
-export default function App() {
-  const [showHomePage, setShowHomePage] = useState(false);
-
-  StatusBar.setBarStyle('light-content', true);
-  StatusBar.setBackgroundColor(COLORS.primary);
-
-  const buttonLabel = (label) => {
-    return (
-      <View style={{
-        padding: 12
-      }}>
-        <Text style={{
-          color: COLORS.title,
-          fontWeight: '600',
-          fontSize: SIZES.h4,
-        }}>
-          {label}
-        </Text>
-      </View>
-    )
-  }
-
-  if (!showHomePage) {
-    return (
-      <AppIntroSlider
-        data={slides}
-        renderItem={({ item }) => {
-          return (
-            <View style={{
-              flex: 1,
-              alignItems: 'center',
-              padding: 15,
-              paddingTop: 100,
-            }}>
-              <Image
-                source={item.image}
-                style={{
-                  width: SIZES.width - 80,
-                  height: 400,
-                }}
-                resizeMode="contain"
-              />
-              <Text style={{
-                fontWeight: 'bold',
-                color: COLORS.title,
-                fontSize: SIZES.h1,
-              }}>
-                {item.title}
-              </Text>
-              <Text style={{
-                textAlign: 'center',
-                paddingTop: 5,
-                color: COLORS.title
-              }}>
-                {item.description}
-              </Text>
-            </View>
-          )
-        }}
-        activeDotStyle={{
-          backgroundColor: COLORS.primary,
-          width: 30,
-        }}
-        showSkipButton
-        renderNextButton={() => buttonLabel("Next")}
-        renderSkipButton={() => buttonLabel("Skip")}
-        renderDoneButton={() => buttonLabel("Done")}
-        onDone={() => {
-          setShowHomePage(true);
-        }}
-      />
-    )
-  }
-
+const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Text>HomeScreen</Text>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
+};
+
+// eslint-disable-next-line no-unused-vars
+const Home = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
-  )
-}
+  );
+};
+
+export default App;
---
+++ src/router/router.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+/* eslint-disable eol-last */
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import OnboardingScreen from '../screens/OnboardingScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+      </Stack.Navigator>
+    );
+  };
+
+export default Router;
