b478301f13c0499cad653a486996b7ce21d2dd3d
NguyenVanChuc09 <chucnguyen116@gmail.com>
2023-11-04 23:03:31 +0700

    m

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import { FlatList, Image, TextInput, ScrollView, StyleSheet, Text, TouchableHigh
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { COLORS } from '../constants/theme';
 import ImagePicker from 'react-native-image-crop-picker';
-import FastImage from 'react-native-fast-image';  
+import FastImage from 'react-native-fast-image';
 
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
@@ -16,90 +16,95 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const URL_IMG = "https://manofmany.com/wp-content/uploads/2021/05/Best-Short-Hairstyles-for-Men.jpg";
 
+const data = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]; // Dữ liệu của bạn
+
 const HomeScreen = () => {
 
   const [password, setPassword] = useState('');
+  const numColumns = Math.ceil(data.length / 2);
 
   const [isFocusedPass, setIsFocusedPass] = useState(false);
 
   return (
-    <ScrollView>
-      <SafeAreaView style={{ paddingVertical: 18, gap: 16, backgroundColor: 'white' }}>
-        {/* Header */}
-        <View style={{ paddingHorizontal: 18, flexDirection: 'row', alignItems: 'center', gap: 8 }}>
-          <Image source={{ uri: URL_IMG }} style={{ width: 52, aspectRatio: 1, borderRadius: 52 }} />
-          <View style={{ flex: 1 }}>
-            <Text style={{ fontSize: 14, marginBottom: 5, color: COLORS.grey, }} numberOfLines={1}>
-              Good Day 👋
-            </Text>
-            <Text style={{ fontSize: 18, color: COLORS.black, fontWeight: "600", }} numberOfLines={1}>
-              Nguyễn Văn Chức
-            </Text>
-          </View>
-          <TouchableOpacity
-            style={{
-              width: 52,
-              aspectRatio: 1,
-              borderRadius: 52,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: 1,
-              borderColor: COLORS.grey,
-            }}>
-            <Fontisto name='bell' size={24} color={COLORS.black} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              width: 52,
-              aspectRatio: 1,
-              borderRadius: 52,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: 1,
-              borderColor: COLORS.grey,
-            }}>
-            <FontAwesome name='send-o' size={24} color={COLORS.black} />
-          </TouchableOpacity>
-        </View>
 
-        {/* Search */}
+    <SafeAreaView style={{ paddingVertical: 18, gap: 16, backgroundColor: 'white' }}>
+      {/* Header */}
+      <View style={{ paddingHorizontal: 18, flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+        <Image source={{ uri: URL_IMG }} style={{ width: 52, aspectRatio: 1, borderRadius: 52 }} />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 14, marginBottom: 5, color: COLORS.grey, }} numberOfLines={1}>
+            Good Day 👋
+          </Text>
+          <Text style={{ fontSize: 18, color: COLORS.black, fontWeight: "600", }} numberOfLines={1}>
+            Nguyễn Văn Chức
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            width: 52,
+            aspectRatio: 1,
+            borderRadius: 52,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: COLORS.grey,
+          }}>
+          <Fontisto name='bell' size={24} color={COLORS.black} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: 52,
+            aspectRatio: 1,
+            borderRadius: 52,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: COLORS.grey,
+          }}>
+          <FontAwesome name='send-o' size={24} color={COLORS.black} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Search */}
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 18,
+          gap: 12,
+        }}>
         <View
           style={{
+            flex: 1,
             flexDirection: 'row',
+            height: 50,
+            borderRadius: 15,
+            alignItems: 'center',
             paddingHorizontal: 18,
-            gap: 12,
+            backgroundColor: "#F5F5F5",
+            marginBottom: 30,
+            backgroundColor: !isFocusedPass ? COLORS.lightGrey : COLORS.blue,
+            borderWidth: 1,
+            borderColor: !isFocusedPass ? COLORS.white : COLORS.primary
           }}>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              height: 50,
-              borderRadius: 15,
-              alignItems: 'center',
-              paddingHorizontal: 18,
-              backgroundColor: "#F5F5F5",
-              marginBottom: 30,
-              backgroundColor: !isFocusedPass ? COLORS.lightGrey : COLORS.blue,
-              borderWidth: 1,
-              borderColor: !isFocusedPass ? COLORS.white : COLORS.primary
-            }}>
-            <AntDesign name='search1' size={24} color={COLORS.grey} />
-            <TextInput
-              placeholder="Search . . ."
-              value={password}
-              onChangeText={(value) => {
-                setPassword(value)
-              }}
-              onFocus={() => { setIsFocusedPass(!isFocusedPass) }}
-              onBlur={() => { setIsFocusedPass(!isFocusedPass) }}
-              style={{ flex: 1, fontSize: 16, color: COLORS.black, paddingHorizontal: 10, }} />
-            <TouchableOpacity onPress={() => {
+          <AntDesign name='search1' size={24} color={COLORS.grey} />
+          <TextInput
+            placeholder="Search . . ."
+            value={password}
+            onChangeText={(value) => {
+              setPassword(value)
+            }}
+            onFocus={() => { setIsFocusedPass(!isFocusedPass) }}
+            onBlur={() => { setIsFocusedPass(!isFocusedPass) }}
+            style={{ flex: 1, fontSize: 16, color: COLORS.black, paddingHorizontal: 10, }} />
+          <TouchableOpacity onPress={() => {
 
-            }}>
-              <Ionicons name='filter' size={24} color={COLORS.primary} />
-            </TouchableOpacity>
-          </View>
+          }}>
+            <Ionicons name='filter' size={24} color={COLORS.primary} />
+          </TouchableOpacity>
         </View>
+      </View>
+
+      <ScrollView>
 
         <View style={{ paddingHorizontal: 18 }}>
           {/* Title */}
@@ -110,12 +115,12 @@ const HomeScreen = () => {
             </TouchableOpacity>
           </View>
 
-          
         </View>
-
         
-      </SafeAreaView>
-    </ScrollView>
+      </ScrollView>
+
+    </SafeAreaView>
+
   )
 }
 
