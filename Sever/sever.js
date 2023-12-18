2f37323575b5147cfdc38a8b3f17120cb1d3d101
NguyenHoang1602 <sakuraimusic123@gmail.com>
2023-12-18 23:53:33 +0700

    oke

--- Sever/sever.js
+++ Sever/sever.js
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
 
-//export const API = 'http://192.168.148.160:3000';
-export const API = 'http://192.168.1.19:3000';
+export const API = 'http://192.168.148.160:3000';
+// export const API = 'http://192.168.1.19:3000';
+// export const API = 'http://10.0.47.187:3000';
