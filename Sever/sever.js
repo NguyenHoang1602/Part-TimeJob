831922a796d06a2dd4a074920a8ef2425e89dbd9
NguyenVanChuc09 <chucnguyen116@gmail.com>
2023-12-20 07:38:12 +0700

    ok

--- Sever/sever.js
+++ Sever/sever.js
@@ -2,4 +2,4 @@
 
 // export const API = 'http://192.168.229.160:3000';
 // export const API = 'http://192.168.1.11:3000';
-export const API = 'http://10.0.62.105:3000';
+export const API = 'http://192.168.200.160:3000';
