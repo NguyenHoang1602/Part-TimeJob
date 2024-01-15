c137fd18e9d6e5bc4a9f8139cf1b8eddf8712557
pinkaanz <104152760+longdeokinh02@users.noreply.github.com>
2024-01-15 11:59:00 +0700

    abc

--- Sever/sever.js
+++ Sever/sever.js
@@ -2,4 +2,4 @@
 
 // export const API = 'http://192.168.229.160:3000';
 // export const API = 'http://192.168.1.11:3000';
-export const API = 'http://192.168.1.28:3000';
+export const API = 'http://192.168.11.35:3000';
